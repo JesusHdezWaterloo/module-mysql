bfa5354017e9668f9db769559f3818b5dbbf16cb
Unknown <unknown@example.com>
2021-01-08 18:14:02 -0500

    Licence a clases

--- src/main/java/com/root101/module/util/mysql/core/usecase_impl/MySQLUseCaseImpl.java
+++ src/main/java/com/root101/module/util/mysql/core/usecase_impl/MySQLUseCaseImpl.java
@@ -1,15 +1,31 @@
-package com.jhw.module.util.mysql.core.usecase_impl;
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.root101.module.util.mysql.core.usecase_impl;
 
 import com.root101.clean.core.app.services.ExceptionHandler;
 import com.root101.clean.core.app.services.NotificationHandler;
 import com.root101.clean.core.app.services.NotificationsGeneralType;
 import com.root101.clean.core.app.usecase.DefaultReadWriteUseCase;
 import com.root101.clean.core.domain.services.ResourceHandler;
-import com.jhw.module.util.mysql.core.domain.Configuration;
-import com.jhw.module.util.mysql.core.module.MySQLCoreModule;
+import com.root101.module.util.mysql.core.domain.Configuration;
+import com.root101.module.util.mysql.core.module.MySQLCoreModule;
 import javax.inject.Inject;
-import com.jhw.module.util.mysql.core.repo_def.MySQLRepo;
-import com.jhw.module.util.mysql.core.usecase_def.MySQLUseCase;
+import com.root101.module.util.mysql.core.repo_def.MySQLRepo;
+import com.root101.module.util.mysql.core.usecase_def.MySQLUseCase;
 import com.root101.utils.others.Network;
 import java.io.File;
 import java.time.LocalDate;
@@ -17,6 +33,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+/**
+ *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ */
 public class MySQLUseCaseImpl extends DefaultReadWriteUseCase<Configuration> implements MySQLUseCase {
 
     public static final int DELAY = 3 * 1000;
@@ -77,7 +98,7 @@ public class MySQLUseCaseImpl extends DefaultReadWriteUseCase<Configuration> imp
             if (resp == 0) {
                 NotificationHandler.showNotification(NOTIFICATION_SALVA_DB,
                         ResourceHandler.getString(MSG_SAVED));
-                    firePropertyChange(PROPERTY_SAVED, false, true);
+                firePropertyChange(PROPERTY_SAVED, false, true);
             }
         } catch (Exception e) {
             Exception ex = new Exception(ResourceHandler.getString(MSG_NO_SAVED));
